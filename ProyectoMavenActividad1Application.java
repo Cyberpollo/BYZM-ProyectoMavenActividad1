f8ed72e8b14ddbefa702e034ef4f88eb26d29f24
Brittany Zacarias <brittayaretzyzacarias@gmail.com>
2026-09-20 00:08:45 -0700

    Add files via upload

---
+++ ProyectoMavenActividad1Application.java
@@ -0,0 +1,13 @@
+package com.si.ProyectoMavenActividad1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoMavenActividad1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoMavenActividad1Application.class, args);
+	}
+
+}
